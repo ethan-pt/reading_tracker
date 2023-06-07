3fff19d3f158180dd5b4bb60c1523fee407d065a
ethan-pt <tubbeethan@gmail.com>
2023-06-07 13:38:12 -0700

    add first pass at books model

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+
+class Books(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(blank=True, null=True)
+    complete = models.BooleanField(default=False)
+    total_pages = models.IntegerField()
+    current_page = models.IntegerField(default=0, validators=[MaxValueValidator(total_pages), MinValueValidator(0)])
+    date = models.DateField(auto_now_add=True)
