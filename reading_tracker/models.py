5fda87416c88af8481328691dce77c6e69ef3915
ethan-pt <tubbeethan@gmail.com>
2023-06-07 23:03:17 -0700

    add min value validator to total pages

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -9,6 +9,6 @@ class Books(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(blank=True, null=True)
     complete = models.BooleanField(default=False)
-    total_pages = models.IntegerField()
+    total_pages = models.IntegerField(validators=[MinValueValidator(0)])
     current_page = models.IntegerField(default=0, validators=[MaxValueValidator(total_pages), MinValueValidator(0)])
     date = models.DateField(auto_now_add=True)
