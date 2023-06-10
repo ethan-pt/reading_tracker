db45abe9234ec9316ca7dcce33a2c0a816e98e18
ethan-pt <tubbeethan@gmail.com>
2023-06-09 23:25:00 -0700

    remove validator

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator
 
 
 
@@ -10,5 +9,5 @@ class Books(models.Model):
     description = models.TextField(blank=True, null=True)
     complete = models.BooleanField(default=False)
     total_pages = models.PositiveIntegerField(default=0)
-    current_page = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(total_pages)])
+    current_page = models.PositiveIntegerField(default=0)
     date = models.DateField(auto_now_add=True)
