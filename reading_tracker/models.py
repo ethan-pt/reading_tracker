39516a05010cdf2aa44ca35767e615b2920a441b
ethan-pt <tubbeethan@gmail.com>
2023-06-09 21:46:55 -0700

    change description to TextField

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -7,7 +7,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Books(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    description = models.CharField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     complete = models.BooleanField(default=False)
     total_pages = models.IntegerField(validators=[MinValueValidator(0)])
     current_page = models.IntegerField(default=0, validators=[MaxValueValidator(total_pages), MinValueValidator(0)])
