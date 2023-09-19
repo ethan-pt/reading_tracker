d6aab16f7a1f58c968b302704287d007093d8da8
ethan-pt <tubbeethan@gmail.com>
2023-09-19 12:57:34 -0700

    refactor book model

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -1,30 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-from django.core.exceptions import ValidationError
 
 
 
-class Books(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    complete = models.BooleanField(default=False)
-    total_pages = models.PositiveIntegerField(default=0)
-    current_page = models.PositiveIntegerField(default=0)
-    date_started = models.DateField(default=timezone.now, editable=True)
-    date_finished = models.DateField(editable=True, blank=True, null=True)
-
-    class Meta:
-        ordering = ['-date_started']
-
-    def clean(self):
-        if self.current_page:
-            if self.current_page > self.total_pages:
-                raise ValidationError('Current page cannot exceed total pages.')
-        
-            if self.current_page != self.total_pages and self.complete:
-                raise ValidationError('Make sure current page reflects book status.')
-        
-        if self.date_finished and self.date_started > self.date_finished:
-            raise ValidationError('Date started cannot exceed date finished.')
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    length = models.PositiveIntegerField()
