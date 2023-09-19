8d8d1cfb0dc1147c037eefaed01cbed2c3a4ff62
ethan-pt <tubbeethan@gmail.com>
2023-09-18 19:10:59 -0700

    add current year context variable

--- reading_tracker/views.py
+++ reading_tracker/views.py
@@ -49,6 +49,7 @@ class ReaderList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['book'] = context['book'].filter(user=self.request.user)
+        context['current_year'] = datetime.date.today().year
         context['finished_count'] = context['book'].filter(date_finished__year=datetime.date.today().year).count()
 
         # search function
