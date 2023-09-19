914f3ea2b6bdf4f83277b591eb42207914fb1fd7
ethan-pt <tubbeethan@gmail.com>
2023-09-19 13:04:12 -0700

    add reading status model

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -8,3 +8,15 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
     length = models.PositiveIntegerField()
+
+class ReadingStatus(models.Model):
+    STATUS_CHOICES = [
+        ('Reading', 'Reading'),
+        ('Finished!', 'Finished!'),
+        ('Gave Up', 'Gave Up'),
+        ('Want to Read', 'Want to Read'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
