6fb356ca4e6813dfaced1a1f02d2aaf29bcbbaf4
ethan-pt <tubbeethan@gmail.com>
2023-09-19 23:00:36 -0700

    add support for different book formats

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 
 
@@ -7,7 +8,8 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     isbn = models.CharField(max_length=13, unique=True)
-    length = models.PositiveIntegerField()
+    length_pages = models.PositiveIntegerField(blank=True)
+    length_time = models.DurationField(blank=True)
 
 class ReadingStatus(models.Model):
     STATUS_CHOICES = [
@@ -22,7 +24,16 @@ class ReadingStatus(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
 
 class ReadingProgress(models.Model):
+    PROGRESS_CHOICES = [
+        ('Page', 'Page')
+        ('Percentage', 'Percentage'),
+        ('Time', 'Time')
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    current_page = models.PositiveIntegerField(help_text="Page number for physical/digital books, percentage for audiobooks")
+    tracking_type = models.CharField(max_length=20, choices=PROGRESS_CHOICES)
+    current_percent = models.PositiveIntegerField(validators=[MaxValueValidator(1)], blank=True)
+    current_page = models.PositiveIntegerField(blank=True)
+    current_time = models.DurationField(blank=True)
     last_updated = models.DateTimeField(auto_now=True)
