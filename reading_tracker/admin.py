39be7bfdc4d0afcf7a8280f2ab6b65e0bc994e95
ethan-pt <tubbeethan@gmail.com>
2023-09-28 12:22:49 -0700

    register models with admin

--- reading_tracker/admin.py
+++ reading_tracker/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Books
+from .models import Book, ReadingStatus, ReadingProgress
 
 
 
-admin.site.register(Books)
+admin.site.register(Book)
+admin.site.register(ReadingStatus)
+admin.site.register(ReadingProgress)
