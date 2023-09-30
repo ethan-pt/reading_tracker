ae0f01dbe00a0e8a94b4502e78f6ddb0470d5d7d
ethan-pt <tubbeethan@gmail.com>
2023-09-30 13:31:18 -0700

    refactor book model tests: include user, use setup

--- reading_tracker/test_models.py
+++ reading_tracker/test_models.py
@@ -7,17 +7,22 @@ from datetime import timedelta
 
 
 class BookModelTest(TestCase):
-    def test_create_paper_book(self):
-        book = Book.objects.create(
-            title="Test Paper Book",
-            author="Test Author",
-            publisher="Test Publisher",
-            isbn="1234567890123",
-            book_type="paper-book",
-            length_pages=200,
+    def setUp(self):
+        self.user = User.objects.create_user(username='testusername', password='testpassword')
+        self.book = Book.objects.create(
+            user=self.user,
+            title='Test Book',
+            author='Test Author',
+            publisher='Test Publisher',
+            isbn='1234567890123',
         )
 
-        self.assertEqual(book.title, 'Test Paper Book')
+    def test_create_paper_book(self):
+        book = self.book
+        book.book_type = 'paper-book'
+        book.length_pages = 200
+
+        self.assertEqual(book.title, 'Test Book')
         self.assertEqual(book.author, 'Test Author')
         self.assertEqual(book.publisher, 'Test Publisher')
         self.assertEqual(book.isbn, '1234567890123')
@@ -26,15 +31,10 @@ class BookModelTest(TestCase):
         self.assertIsNone(book.length_time)
 
     def test_create_ebook(self):
-        book = Book.objects.create(
-            title="Test e-book",
-            author="Test Author",
-            publisher="Test Publisher",
-            isbn="1234567890123",
-            book_type="e-book",
-        )
+        book = self.book
+        book.book_type = 'e-book'
 
-        self.assertEqual(book.title, 'Test e-book')
+        self.assertEqual(book.title, 'Test Book')
         self.assertEqual(book.author, 'Test Author')
         self.assertEqual(book.publisher, 'Test Publisher')
         self.assertEqual(book.isbn, '1234567890123')
@@ -43,19 +43,14 @@ class BookModelTest(TestCase):
         self.assertIsNone(book.length_time)
 
     def test_create_audio_book(self):
-        book = Book.objects.create(
-            title="Test Audio Book",
-            author="Test Author",
-            publisher="Test Publisher",
-            isbn="1234567890123",
-            book_type="audio-book",
-            length_time=timedelta(
-                hours=2,
-                minutes=30
-            ),
+        book = self.book
+        book.book_type = 'audio-book'
+        book.length_time = timedelta(
+            hours=2,
+            minutes=30,
         )
 
-        self.assertEqual(book.title, 'Test Audio Book')
+        self.assertEqual(book.title, 'Test Book')
         self.assertEqual(book.author, 'Test Author')
         self.assertEqual(book.publisher, 'Test Publisher')
         self.assertEqual(book.isbn, '1234567890123')
