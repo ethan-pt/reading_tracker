72c3dcf0498506b1eddf7f8ac4a6081e97644fcc
ethan-pt <tubbeethan@gmail.com>
2023-10-02 14:45:59 -0700

    create tests module

--- reading_tracker/tests/test_models.py
+++ reading_tracker/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Book, ReadingStatus, ReadingProgress
+from ..models import Book, ReadingStatus, ReadingProgress
 
 from datetime import timedelta
 
