5e0b90263c3902b4f5038ef23e7fd5c4ee3ddb69
ethan-pt <tubbeethan@gmail.com>
2023-10-04 16:59:05 -0700

    add readerregister view tests

--- reading_tracker/tests/test_views.py
+++ reading_tracker/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -39,5 +40,54 @@ class ReaderLoginViewTest(TestCase):
         self.client.login(username='testuser', password='testpassword')
 
         response = self.client.get(reverse('login'))
+
+        self.assertRedirects(response, reverse('reader'))
+
+class ReaderRegisterViewTest(TestCase):
+    def test_register_template(self):
+        """
+        Ensure register url returns correct template
+        """
+        response = self.client.get(reverse('register'))
+        self.assertTemplateUsed(response, 'reading_tracker/register.html')
+    
+    def test_register_validation(self):
+        """
+        Tests user registration with valid data and checks for successful registration, user existence, and authentication
+        """
+        response = self.client.post(reverse('register'), {
+            'username':'testuser',
+            'password1':'testpassword',
+            'password2':'testpassword'
+        })
+
+        self.assertRedirects(response, reverse('reader'))
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+        authenticated_user = authenticate(username='testuser', password='testpassword')
+        self.assertIsNotNone(authenticated_user)
+        self.assertTrue(authenticated_user.is_authenticated)
+
+    def test_register_invalidation(self):
+        """
+        Tests user registration with invalid data
+        """
+        response = self.client.post(reverse('register'), {
+            'username':'testuser',
+            'password1':'testpassword',
+            'password2':'youjustlostthegame'
+        })
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_redirection(self):
+        """
+        Ensures authenticated users are redirected to reader template when trying to access register view
+        """
+        User.objects.create_user(username='testuser', password='testpassword')
+
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.get(reverse('register'))
 
         self.assertRedirects(response, reverse('reader'))
