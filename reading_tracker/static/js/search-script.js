d66ae154adeb61c85e3ae8ca0578aba923c018ea
Assentt <tubbeethan@gmail.com>
2023-11-15 00:14:28 -0800

    move request functions, and some edits for readability

--- reading_tracker/static/js/search-script.js
+++ reading_tracker/static/js/search-script.js
@@ -5,27 +5,33 @@ searchButton.addEventListener("click", (event) => {
     let searchText = encodeURIComponent(document.getElementById("search-book-area").value);
 
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", searchPath + searchText);
-    console.log('request sent...');
     xhr.onreadystatechange = () => {
         console.log(xhr.status);
         if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log('success')
+            console.log('success');
+
             const contentWrapper = document.getElementById("content");
             const data = JSON.parse(xhr.response);
 
             for(let i = 0; i < data.items.length; i++) {
-                let item = data.items[i];
                 console.log(item.volumeInfo.title);
+                
+                let item = data.items[i];
                 const bookDiv = document.createElement("div");
                 bookDiv.innerHTML = `<br><p>${item.volumeInfo.title}</p>`;
                 contentWrapper.appendChild(bookDiv);
             }
         } else {
-            console.log('failure')
+            console.log('failure');
+
             const errMsg = document.createElement("h4");
             errMsg.innerHTML = "Something went wrong, try again later.";
             contentWrapper.appendChild(errMsg);
         }
     }
+
+    xhr.open("GET", searchPath + searchText);
+    xhr.send();
+
+    console.log('request sent...');
 });
