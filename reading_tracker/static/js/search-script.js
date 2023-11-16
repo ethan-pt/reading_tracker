04b80ffbc877c1400c58df13b84bf9642a36b790
ethan-pt <tubbeethan@gmail.com>
2023-11-16 13:17:33 -0800

    remove console.logs, move contentWrapper to before first call

--- reading_tracker/static/js/search-script.js
+++ reading_tracker/static/js/search-script.js
@@ -1,29 +1,22 @@
 const searchButton = document.getElementById("search-book-button");
 searchButton.addEventListener("click", (event) => {
-    console.log('button pressed');
     const searchPath = "https://www.googleapis.com/books/v1/volumes?q=";
     let searchText = encodeURIComponent(document.getElementById("search-book-area").value);
 
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
-        console.log(xhr.status);
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log('success');
+        const contentWrapper = document.getElementById("content");
 
-            const contentWrapper = document.getElementById("content");
+        if (xhr.readyState == 4 && xhr.status == 200) {
             const data = JSON.parse(xhr.response);
 
             for(let i = 0; i < data.items.length; i++) {
-                console.log(item.volumeInfo.title);
-                
                 let item = data.items[i];
                 const bookDiv = document.createElement("div");
                 bookDiv.innerHTML = `<br><p>${item.volumeInfo.title}</p>`;
                 contentWrapper.appendChild(bookDiv);
             }
         } else {
-            console.log('failure');
-
             const errMsg = document.createElement("h4");
             errMsg.innerHTML = "Something went wrong, try again later.";
             contentWrapper.appendChild(errMsg);
@@ -32,6 +25,4 @@ searchButton.addEventListener("click", (event) => {
 
     xhr.open("GET", searchPath + searchText);
     xhr.send();
-
-    console.log('request sent...');
 });
