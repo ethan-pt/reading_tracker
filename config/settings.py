773dc053fd2e7130fd2518c28899e886999f9251
ethan-pt <tubbeethan@gmail.com>
2023-11-25 13:06:18 -0800

    add allowed origin and move cors middleware to top of list

--- config/settings.py
+++ config/settings.py
@@ -83,19 +83,20 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'https://www.googleapis.com',
     'https://books.google.com',
+    'http://127.0.0.1:8080',
 ]
 
 
