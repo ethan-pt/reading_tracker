9c2b510bd26bc2477a278466cd49f5dc41c802eb
ethan-pt <tubbeethan@gmail.com>
2023-12-27 14:16:17 -0800

    refactor to use django scripting where possible

--- reading_tracker/static/js/search-script.js
+++ reading_tracker/static/js/search-script.js
@@ -1,76 +1,9 @@
-// upon search event, request search results from Google Books API
-const searchForm = document.getElementById("search-form");
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const request = new XMLHttpRequest();
-    request.onload = () => {
-        const contentWrapper = document.getElementById("content");
-        contentWrapper.innerHTML = "";
-        contentWrapper.appendChild(document.createElement("br"));
-
-        // displays book title, cover, and author(s) if request is good
-        if (request.readyState == 4 && request.status == 200) {
-            const bookData = JSON.parse(request.response);
-
-            // for book in book search results, display book
-            for(let i = 0; i < bookData.items.length; i++) {
-                let item = bookData.items[i];
-                const bookId = item.id; // returns an id string
-                const bookTitle = document.createElement("h4");
-                bookTitle.innerHTML = item.volumeInfo.title; // returns a title string
-                const bookAuthors  = document.createElement("p");
-                bookAuthors.innerHTML = `<strong>By:</strong> ${item.volumeInfo.authors.join(", ")}`; // returns a list of authors
-
-                // if Google Books has a cover image, display image, else display default image
-                const bookCover = document.createElement("img");
-                const imgUrl = `https://books.google.com/books/content?id=${bookId}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api`; // this is a direct path to book cover thumbnail image
-                if (imgUrl) {
-                    bookCover.src = imgUrl;
-                } else {
-                    bookCover.src = coverNotFoundImg; // coverNotFoundImg declaration located in book_search template
-                }
-
-                // set image height and allow width to maintain aspect ratio, using new width to calculate padding width to keep a square border
-                bookCover.style.height = "80px";
-                bookCover.style.width = "auto";
-                bookCover.onload = (event) => {
-                    coverDiv.style.padding = `0 ${45-(bookCover.width/2)}px`;
-                }
-
-                // add each book's data to a div and each div to another div
-                const resultsDiv = document.createElement("div");
-                resultsDiv.setAttribute("class", "results-div");
-
-                const coverDiv = document.createElement("div");
-                coverDiv.setAttribute("class", "cover-div");
-                coverDiv.appendChild(bookCover);
-                resultsDiv.appendChild(coverDiv);
-
-                const infoDiv = document.createElement("div");
-                infoDiv.setAttribute("class", "info-div");
-                infoDiv.appendChild(bookTitle);
-                infoDiv.appendChild(bookAuthors);
-                resultsDiv.appendChild(infoDiv);
-
-                const anchorWrapper = document.createElement("a");
-                anchorWrapper.setAttribute("href", "");
-                anchorWrapper.appendChild(resultsDiv);
-
-                contentWrapper.appendChild(anchorWrapper);
-            }
-        // displays error message if request is bad
-        } else { 
-            const errMsg = document.createElement("h4");
-            errMsg.innerHTML = "Something went wrong, try again later.";
-            contentWrapper.appendChild(errMsg);
-        }
+const coverDiv = document.getElementsByClassName('cover-div');
+const coverImg = document.getElementsByClassName('cover-img');
+// iterates through each cover div and calculates padding needed 
+// based on cover image width to square off cover div outline
+for (let i = 0; i < coverDiv.length; i++) {
+    coverImg[i].onload = (event) => {
+        coverDiv[i].style.padding = `0 ${45 - (coverImg[i].width / 2)}px`;
     }
-
-    // get form data
-    const formData = new FormData(searchForm);
-    const searchText = encodeURIComponent(formData.get("text-area"));
-
-    request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=" + searchText);
-    request.send();
-});
+}
