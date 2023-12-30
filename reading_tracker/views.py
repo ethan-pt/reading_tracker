31a6da58c4a90bb91aba4a9a7620c958371dcc27
ethan-pt <tubbeethan@gmail.com>
2023-12-30 13:18:05 -0800

    add result form handling

--- reading_tracker/views.py
+++ reading_tracker/views.py
@@ -67,36 +67,45 @@ class ReaderSearch(LoginRequiredMixin, FormView):
     form_class = SearchForm
 
     def form_valid(self, form):
-        search_query = form.cleaned_data['search_query']
-        api_url = f'https://www.googleapis.com/books/v1/volumes?q={urllib.parse.quote_plus(search_query)}'
-        response = requests.get(api_url)
+        # if posted form is search form, do search form stuff, else if form is result form, do result form stuff
+        if form.fields['search_query']:
+            search_query = form.cleaned_data['search_query']
+            api_url = f'https://www.googleapis.com/books/v1/volumes?q={urllib.parse.quote_plus(search_query)}'
+            response = requests.get(api_url)
 
-        # if request is successful, return request data, else return false success bool so front end knows to display error message
-        if response.status_code == 200:
-            data = response.json()
-            book_count = data.get('totalItems')
-            books = data.get('items')
+            # if request is successful, return request data, else return false success bool so front end knows to display error message
+            if response.status_code == 200:
+                data = response.json()
+                book_count = data.get('totalItems')
+                books = data.get('items')
 
-            # this for loop iterates through book objects returned and adds cover photo and 
-            # replaces authors list with a joined string for each book in returned book data
-            for book in books:
-                book['volumeInfo']['authors'] = ', '.join(book['volumeInfo']['authors'])
-                book['cover'] = f"https://books.google.com/books/content?id={book['id']}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+                # this for loop iterates through book objects returned and adds cover photo and 
+                # replaces authors list with a joined string for each book in returned book data
+                for book in books:
+                    book['volumeInfo']['authors'] = ', '.join(book['volumeInfo']['authors'])
+                    book['cover'] = f"https://books.google.com/books/content?id={book['id']}&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
 
-            context = {
-                'form': SearchForm,
-                'success_bool': True, # tells front end whether or not search request was successful
-                'book_count': book_count,
-                'books': books,
-            }
+                context = {
+                    'form': SearchForm,
+                    'success_bool': True, # tells front end whether or not search request was successful
+                    'book_count': book_count,
+                    'books': books,
+                }
+            
+            else:
+                context = {
+                    'form': SearchForm,
+                    'success_bool': False
+                }
+
+            return render(self.request, self.template_name, context)
         
-        else:
-            context = {
-                'form': SearchForm,
-                'success_bool': False
-            }
+        # if results form, set session variable to form data (the user's chosen book) and redirect to ReaderCreate
+        elif form.fields['book-id']:
+            self.request.session['result_data'] = form.cleaned_data['book-id']
 
-        return render(self.request, self.template_name, context)
+            redirect('book-create')
+
 
 
 class ReaderCreate(LoginRequiredMixin, CreateView):
