9164068285a44469f58c65983af8043e0aed8760
ethan-pt <tubbeethan@gmail.com>
2024-01-07 02:14:32 -0800

    remove js file

--- reading_tracker/static/js/search-script.js
+++
@@ -1,9 +0,0 @@
-const coverDiv = document.getElementsByClassName('cover-div');
-const coverImg = document.getElementsByClassName('cover-img');
-// iterates through each cover div and calculates padding needed 
-// based on cover image width to square off cover div outline
-for (let i = 0; i < coverDiv.length; i++) {
-    coverImg[i].onload = (event) => {
-        coverDiv[i].style.padding = `0 ${45 - (coverImg[i].width / 2)}px`;
-    }
-}
