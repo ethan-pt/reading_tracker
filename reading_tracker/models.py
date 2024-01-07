458c5acfee90c6d24920a3ffdad9a4c97bf8fefe
ethan-pt <tubbeethan@gmail.com>
2024-01-07 02:35:18 -0800

    reorder book model fields

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -12,11 +12,11 @@ class Book(models.Model):
     ]
 
     cover_url = models.URLField(blank=True, null=True)
+    gbooks_id = models.CharField(max_length=12, unique=True, blank=True, null=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     publisher = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    gbooks_id = models.CharField(max_length=12, unique=True, blank=True, null=True)
     book_type = models.CharField(choices=BOOK_TYPE_CHOICES)
     length_pages = models.PositiveIntegerField(blank=True, null=True)
     length_time = models.DurationField(blank=True, null=True)
