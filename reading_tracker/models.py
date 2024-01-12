4beaab85c9cdb035dc7dd49131f7f0f45701de18
ethan-pt <tubbeethan@gmail.com>
2024-01-12 13:40:15 -0800

    allow publisher field to be empty

--- reading_tracker/models.py
+++ reading_tracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
+from pkg_resources import require
 
 
 
@@ -15,7 +16,7 @@ class Book(models.Model):
     gbooks_id = models.CharField(max_length=12, unique=True, blank=True, null=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    publisher = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     book_type = models.CharField(choices=BOOK_TYPE_CHOICES)
     length_pages = models.PositiveIntegerField(blank=True, null=True)
