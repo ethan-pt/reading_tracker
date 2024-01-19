fdc04863375da4ade98eecef5ec529eb82d6c978
ethan-pt <tubbeethan@gmail.com>
2024-01-19 15:53:47 -0800

    remove readingstatus model

--- reading_tracker/admin.py
+++ reading_tracker/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Book, ReadingStatus, ReadingProgress
+from .models import Book, ReadingProgress
 
 
 
 admin.site.register(Book)
-admin.site.register(ReadingStatus)
 admin.site.register(ReadingProgress)
