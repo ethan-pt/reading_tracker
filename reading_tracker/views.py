dcbe26ab96590555b834cb11b55206316485cc88
ethan-pt <tubbeethan@gmail.com>
2024-01-20 13:42:31 -0800

    add progress to list view context

--- reading_tracker/views.py
+++ reading_tracker/views.py
@@ -60,6 +60,7 @@ class ReaderList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['books'] = context['books'].filter(user=self.request.user)
+        context['progress'] = ReadingProgress.objects.filter(user=self.request.user)
         return context
 
 
